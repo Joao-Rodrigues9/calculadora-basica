5aef17a9191c10c0c1461b2ad2700c1043a4660b
João Rodrigues <joaovitor550dito@gmail.com>
2026-01-20 16:13:34 -0300

    Feature: funções inicio e menu adicionadas
    coloquei a inserção de numero inicial e a chamada de menu em funções para facilitar o reuso do código caso necessário

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int inicio();
+void menu();
+
 int main()
 {
-    cout << "Indique seu numero:\n";
-    int num;
-    cin >> num;
 
-    cout << "O que deseja fazer?\n"
-         << "0 - Encerrar o programa\n"
-         << "1 - Visualizar o numero\n"
-         << "2 - Multiplicar o numero por dois\n"
-         << "3 - Dividir o numero por dois\n";
+    int valor = inicio();
+    menu();
 
     int escolha;
     cin >> escolha;
@@ -19,21 +16,40 @@ int main()
     switch (escolha)
     {
     case 1:
-        cout << num << "\n";
+        cout << "Valor atual: " << valor << "\n";
         break;
 
     case 2:
-        num = num * 2;
-        cout << num << "\n";
+        valor = valor * 2;
+        cout << "Valor atual: " << valor << "\n";
         break;
 
     case 3:
-        num = num / 2;
-        cout << num << "\n";
+        valor = valor / 2;
+        cout << "Valor atual: " << valor << "\n";
         break;
 
     default:
-        break;
+        cout << "Programa encerrado com sucesso\n";
     }
     return 0;
+}
+
+int inicio()
+{
+    cout << "Indique seu numero:\n";
+    int num;
+    cin >> num;
+
+    return num;
+}
+
+void menu()
+{
+    cout << "O que deseja fazer?\n"
+         << "0 - Encerrar o programa\n"
+         << "1 - Visualizar o numero\n"
+         << "2 - Multiplicar o numero por dois\n"
+         << "3 - Dividir o numero por dois\n"
+         << "-------------------------------\n";
 }
